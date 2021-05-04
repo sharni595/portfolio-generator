24e4a04516c517cc19700f1814f14ef9438a4de8
Sharni Zaugg <sharni.greener@gmail.com>
2021-05-03 18:29:05 -0600

    generate the HTML file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('=============');
+const pageHTML = generatePage(name, github)
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', pageHTML, err =>{
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
